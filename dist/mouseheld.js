e12d3548361665f23c44e4626559c6d1d8196e25
Anoop Elias <anoopelias@gmail.com>
2015-02-03 00:35:42 +0530

    Added missing $ function param

--- dist/mouseheld.js
+++ dist/mouseheld.js
@@ -1,4 +1,4 @@
-(function() {
+(function($) {
     var lastTime = 0;
     var vendors = [ "ms", "moz", "webkit", "o" ];
     for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
--- js/mouseheld.js
+++ js/mouseheld.js
@@ -6,7 +6,7 @@
 // requestAnimationFrame polyfill by Erik Möller
 // fixes from Paul Irish and Tino Zijdel
  
-(function() {
+(function($) {
   var lastTime = 0;
   var vendors = ['ms', 'moz', 'webkit', 'o'];
   for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
